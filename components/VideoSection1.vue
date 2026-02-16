edb4ec9149a33058aeba5b783ba0bfbe8397b47f
chikireet <clesmica@gmail.com>
2026-02-16 14:17:28 +0100

    Update VideoSection1.vue

--- components/VideoSection1.vue
+++ components/VideoSection1.vue
@@ -2,9 +2,9 @@
   <section class="video-section section bg-black relative">
     <div class="video-wrapper">
       <div class="overlay"></div>
-      <video autoplay muted loop playsinline class="video-element">
-        <source src="/previews/chill_your_stress_othership.mp4" type="video/mp4" />
-      </video>
+      <video autoplay muted loop playsinline>
+    <source src="/previews/chill_your_stress_othership.mp4" type="video/mp4">
+  </video>
     </div>
     <div class="absolute z-10 bottom-10 left-10 text-white text-3xl font-bold">
       {{ title }}
