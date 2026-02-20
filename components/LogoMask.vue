2018424bd9714feafa49d06e70f04e78e2c19fc0
chikireet <clesmica@gmail.com>
2026-02-20 21:18:08 +0100

    Update LogoMask.vue

--- components/LogoMask.vue
+++ components/LogoMask.vue
@@ -106,7 +106,7 @@ const updateSize = () => {
   
   // --- PRECISION CONTROLS ---
   // Adjust these specific numbers to nudge the logo for the perfect match
-  const desktopWidth = 151  // Final width in pixels on desktop
+  const desktopWidth = 152  // Final width in pixels on desktop
   const desktopTop   = 19   // Final distance from top edge in pixels on desktop
   
   const mobileWidth  = 100  // Final width in pixels on mobile
