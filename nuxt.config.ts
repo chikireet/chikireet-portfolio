9fcd258657baace0119f377d2411d2d6d5f346d0
chikireet <clesmica@gmail.com>
2026-02-21 21:00:07 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,29 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }
+      ],
+      // META PIXEL INTEGRATION
+      script: [
+        {
+          children: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '27066338849635015');
+            fbq('track', 'PageView');
+          `,
+          type: 'text/javascript'
+        }
+      ],
+      noscript: [
+        {
+          children: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=27066338849635015&ev=PageView&noscript=1"/>`
+        }
       ]
     }
   },
@@ -71,5 +94,5 @@ export default defineNuxtConfig({
     }
   },
 
-  ssr: true 
+  ssm: true 
 })
