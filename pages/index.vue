f96766075ccf36ae06ac5c26b1fb56cef4f403dd
chikireet <clesmica@gmail.com>
2026-02-21 13:08:05 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -63,7 +63,7 @@ import gsap from 'gsap'
 useSeoMeta({
   title: 'Gene Perez | Director & Photographer',
   description: 'Toronto-based Director and Photographer.',
-  ogImage: '/share-preview2.webp',
+  ogImage: '/share-preview3.webp',
 })
 
 const HeroSection = defineAsyncComponent(() => import('@/components/HeroSection.vue'))
